61bf0e7d9735677cc8cfa8b6b1f08373ce3f52ff
Atul Thakur <63915710+ThakurAtul@users.noreply.github.com>
2020-11-04 21:04:20 +0530

    Update YouTube-Video-Downloader.py

--- YouTube-Video-Downloader.py
+++ YouTube-Video-Downloader.py
@@ -4,6 +4,11 @@ url = input("Enter the complete URL of the video")
 
 video = pafy.new(url)
 
+print(video.title) #to get the title of the video
+print(video.author) #to get the author of the video 
+print(video.viewcount) #to get the number of views
+print(video.length) #to get the length of video
+
 streams = video.streams
 
 for i in streams:
