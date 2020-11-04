715157255a181bcad5c16cb8c2642a2c84ffdab0
Atul Thakur <63915710+ThakurAtul@users.noreply.github.com>
2020-11-04 21:09:48 +0530

    Update YouTube-Video-Downloader.py

--- YouTube-Video-Downloader.py
+++ YouTube-Video-Downloader.py
@@ -1,7 +1,8 @@
 import pafy
 
-url = input("Enter the complete URL of the video")
+url = input("Enter the complete URL of the video") # https://youtu.be/gTjXTRz4dDw
 
+# create an object using pafy.new()
 video = pafy.new(url)
 
 print(video.title) #to get the title of the video
@@ -9,12 +10,12 @@ print(video.author) #to get the author of the video
 print(video.viewcount) #to get the number of views
 print(video.length) #to get the length of video
 
-streams = video.streams
+streams = video.streams  #this will return A list of regular streams (streams containing both audio and video)
 
 for i in streams:
-    print (i)
+    print (i) #print all the streams available for that video link
 
-best = video.getbest()
+best = video.getbest() #this method returns the best resolution
 
 print(best)
 best.download()
